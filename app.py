d4b206c70135dc2d2636d6404bc002724acb585a
stud-A230A14 <MSierski>
2022-05-26 11:09:40 +0200

    gallery finish

--- app.py
+++ app.py
@@ -1,23 +1,25 @@
 from flask import Flask, render_template
-from flask_mail import Mail, Message
+# from flask_mail import Mail, Message
+#
+# app = Flask(__name__)
+#
+# app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+# app.config['MAIL_PORT'] = 465
+# app.config['MAIL_USERNAME'] = 'yourId@gmail.com'
+# app.config['MAIL_PASSWORD'] = '*****'
+# app.config['MAIL_USE_TLS'] = False
+# app.config['MAIL_USE_SSL'] = True
+# mail = Mail(app)
+#
+#
+# @app.route("/about")
+# def about():
+#     msg = Message('Hello', sender='yourId@gmail.com', recipients=['someone1@gmail.com'])
+#     msg.body = "Hello Flask message sent from Flask-Mail"
+#     mail.send(msg)
+#     return "Sent"
 
 app = Flask(__name__)
-
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'yourId@gmail.com'
-app.config['MAIL_PASSWORD'] = '*****'
-app.config['MAIL_USE_TLS'] = False
-app.config['MAIL_USE_SSL'] = True
-mail = Mail(app)
-
-
-@app.route("/about")
-def about():
-    msg = Message('Hello', sender='yourId@gmail.com', recipients=['someone1@gmail.com'])
-    msg.body = "Hello Flask message sent from Flask-Mail"
-    mail.send(msg)
-    return "Sent"
 
 
 @app.route('/')
