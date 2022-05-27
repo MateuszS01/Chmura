0705f91f09f6c8ca62a15679e06fa1269929d9df
Mati <wrx71226@student.wsb.wroclaw.pl>
2022-05-27 14:14:22 +0200

    database 1

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+import sqlite3
+
 # from flask_mail import Mail, Message
 #
 # app = Flask(__name__)
@@ -22,6 +24,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -37,9 +45,12 @@ def gallery():
     return app.send_static_file('gallery.html')
 
 
-@app.route('/ksiegagosci')
-def ksiegagosci():
-    return app.send_static_file('ksiegagosci.html')
+@app.route('/guestbook')
+def guestbook():
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
+    return render_template('guestbook.html', posts=posts)
 
 
 if __name__ == '__main__':
--- demo.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-mydb = mysql.connector.connect(
-  host="msbaza.database.windows.net",
-  user="...",
-  password="..."
-)
-
-print(mydb)
